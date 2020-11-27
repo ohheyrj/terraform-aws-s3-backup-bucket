074319e1ae0a4b6993ca80bd97b185602fbe897f
Richard Annand <rj170590@gmail.com>
2020-11-27 08:49:36 +0000

    feat(outputs.tf): added additional output
    added output of access key id

--- outputs.tf
+++ outputs.tf
@@ -8,6 +8,11 @@ output "iam_user" {
   description = "The IAM user created."
 }
 
+output "iam_user_access_key_id" {
+  value = aws_iam_access_key.id
+  description = "The IAM access key id."
+}
+
 output "iam_user_encrypted_secret" {
   value       = aws_iam_access_key.iam_access_key.encrypted_secret
   description = "Encrypted Secret for IAM backup user"
